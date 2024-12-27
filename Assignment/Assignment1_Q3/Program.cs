60d8f7281abcc9c4584c823da81f0cdaeae5baa4
mrunalipardhi2000 <mrunali30pardhi@gmail.com>
2024-12-27 07:55:15 +0530

    Assignment 01 and Assignment02

---
+++ Assignment/Assignment1_Q3/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1_Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2, result = 0;
+            char choice;
+            bool exit = false;
+
+
+            do
+            {
+                Console.WriteLine("\n--------- Basic Calculator-------------");
+                Console.WriteLine("1. Addition (+)");
+                Console.WriteLine("2. Subtraction (-)");
+                Console.WriteLine("3. Multiplication (*)");
+                Console.WriteLine("4. Division (/)");
+                Console.WriteLine("5. Exit");
+                Console.Write("Enter your choice: ");
+                choice = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+                if (choice == '5')
+                {
+                    exit = true;
+                    Console.WriteLine("Exiting the calculator. Goodbye!");
+                    break;
+                }
+
+                Console.Write("Enter the first number: ");
+                while (!double.TryParse(Console.ReadLine(), out num1))
+                {
+                    Console.Write("Invalid input. Please enter a valid number: ");
+                }
+
+                Console.Write("Enter the second number: ");
+                while (!double.TryParse(Console.ReadLine(), out num2))
+                {
+                    Console.Write("Invalid input. Please enter a valid number: ");
+                }
+
+                switch (choice)
+                {
+                    case '1':
+                        result = num1 + num2;
+                        Console.WriteLine($"Result: {num1} + {num2} = {result}");
+                        break;
+
+                    case '2':
+                        result = num1 - num2;
+                        Console.WriteLine($"Result: {num1} - {num2} = {result}");
+                        break;
+
+                    case '3':
+                        result = num1 * num2;
+                        Console.WriteLine($"Result: {num1} * {num2} = {result}");
+                        break;
+
+                    case '4':
+                        if (num2 != 0)
+                        {
+                            result = num1 / num2;
+                            Console.WriteLine($"Result: {num1} / {num2} = {result}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error: Division by zero is not allowed.");
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice. Please select a valid option.");
+                        break;
+                }
+
+            } while (!exit);
+        }
+    }
+
+
+}
+    
+
---
+++ dotnet_assignmnet/Assignment 01/Assignment1_Q2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1_Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string value;
+            do
+            {
+                int result;
+                Console.WriteLine("Enter first number");
+                 int numberFirst = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter second number");
+                int numberSecond= Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter symbol +,- *,/");
+                string operatorCal= Console.ReadLine();
+                switch (operatorCal)
+                {
+                    case "+":
+                        result = numberFirst + numberSecond;
+                        Console.WriteLine("Addition:" + result);
+
+                        break;
+                    case "-":
+                        result = numberFirst - numberSecond;
+                        Console.WriteLine("Subtraction:" + result);
+                        break;
+                    case "*":
+                        result = numberFirst * numberSecond;
+                        Console.WriteLine("Multiplication:" + result);
+                        break;
+                    case "/":
+                        result = numberFirst / numberSecond;
+                        Console.WriteLine("division:" + result);
+                        break;
+                }
+                Console.ReadLine();
+                Console.Write("Do you want to continue(y/n):");
+                value = Console.ReadLine();
+
+            }
+            while (true);
+        }
+    }
+}
